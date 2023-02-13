3c836400c3cf1956e6edcf079054980bfae66f86
lukaszmielczarekdev <lukaszmielczarekdev@gmail.com>
2023-02-13 23:23:17 +0100

    PO-015 deployment

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,10 +2,11 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const API_URL = process.env.API_URL || "http://localhost:5000";
-// guest password to work offline
-const DATABASE_PASSWORD = "g7H3WA4wVSGcaIw1";
-const CONNECTION_URL = `mongodb+srv://Guest:${DATABASE_PASSWORD}@pobieraczobrazkow.7xtizdz.mongodb.net/?retryWrites=true&w=majority`;
+const API_URL =
+  process.env.API_URL ||
+  "https://pobieraczobrazkow-api-production.up.railway.app";
+const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD || "";
+const CONNECTION_URL = `mongodb+srv://:${DATABASE_PASSWORD}@pobieraczobrazkow.7xtizdz.mongodb.net/?retryWrites=true&w=majority`;
 const PORT = process.env.PORT || 5000;
 
 export const config = {
