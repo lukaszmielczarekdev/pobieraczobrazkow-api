1392bde0f40c3bb922b228170f15e180aa259689
lukaszmielczarekdev <lukaszmielczarekdev@gmail.com>
2023-02-13 23:59:17 +0100

    PO-017 database credentials

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,7 +6,7 @@ const API_URL =
   process.env.API_URL ||
   "https://pobieraczobrazkow-api-production.up.railway.app";
 const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD || "";
-const CONNECTION_URL = `mongodb+srv://:${DATABASE_PASSWORD}@pobieraczobrazkow.7xtizdz.mongodb.net/?retryWrites=true&w=majority`;
+const CONNECTION_URL = `mongodb+srv://lukaszmielczarekdev:${DATABASE_PASSWORD}@pobieraczobrazkow.7xtizdz.mongodb.net/?retryWrites=true&w=majority`;
 const PORT = process.env.PORT || 5000;
 
 export const config = {
